46f5e860dc4fd8401e6e9cbabf4dda8e358ddf44
Vinny Tiwari <tiwari.vineet@outlook.com>
2015-05-05 20:22:05 -0700

    Initial Commit

---
+++ MathGame/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  MathGame
+//
+//  Created by Vineet Tiwari on 2015-05-05.
+//  Copyright (c) 2015 vinny.co. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+    return 0;
+}
